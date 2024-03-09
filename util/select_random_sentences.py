e09edef5329abb14d6caa966c088e69641bf540a
Kunal Rout <2kunal6@gmail.com>
2024-03-09 20:49:53 +0530

    replacing multiple spaces with single

--- util/select_random_sentences.py
+++ util/select_random_sentences.py
@@ -6,4 +6,5 @@ lines = text_file.readlines()
 random_list = random.sample(range(0, 10267), 100)
 
 for i in random_list:
-    print(lines[i], end='')
+    line = " ".join(lines[i].split())
+    print(line)
