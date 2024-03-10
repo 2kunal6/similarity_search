320d2bda37f10aa0c1f85f6f857af38b695ed4b5
Kunal Rout <2kunal6@gmail.com>
2024-03-10 14:16:24 +0530

    removing intermediate files

--- util/download_dataset.py
+++ util/download_dataset.py
@@ -3,13 +3,8 @@ from datasets import load_dataset
 dataset = load_dataset("code_search_net", "python")
 df = dataset['train'].to_pandas()
 
-df_slice = df[:10000]
-df_slice['func_documentation_string'] = df_slice['func_documentation_string'].str.replace(r'\n', ' ', regex=True)
-print(df_slice.iloc[0:10])
-with open('../resources/data_raw.txt', 'w') as f:
-    f.write(df_slice['func_documentation_string'].str.cat(sep='\n'))
+df_slice = df[:2000]
+df_slice['func_documentation_string'] = df['func_documentation_string'].apply(lambda x: ' '.join(x.replace('\n', ' ').split()))
 
-text_file = open("resources/data_raw.txt", "r")
-lines = text_file.readlines()
-for line in lines:
-    print(" ".join(line.split()))
+with open('resources/data.txt', 'w') as f:
+    f.write(df_slice['func_documentation_string'].str.cat(sep='\n'))
