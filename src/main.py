580a4f9bc7bbf549259b72ce3b58daa8de54205d
Kunal Rout <2kunal6@gmail.com>
2024-03-10 12:33:37 +0530

    searching from semantically similar sentences

--- src/main.py
+++ src/main.py
@@ -1,8 +1,16 @@
 from faiss_search import FAISSindex
 from vectordb_search import Chroma_DB
 
+import os
+
+
 faiss_search = FAISSindex()
 chroma_db = Chroma_DB()
 
-print(faiss_search.search_similar_docs('dummy', 2))
-print(chroma_db.search_similar_docs('dummy', 2))
+for file in os.listdir('resources/semanticaly_similar_sentences'):
+    text_file = open(f'resources/semanticaly_similar_sentences/{file}')
+    lines = text_file.readlines()
+    print(lines[0])
+    for i in range(2,len(lines)):
+        print(faiss_search.search_similar_docs(lines[i], 1))
+        print(chroma_db.search_similar_docs(lines[i], 1))
--- util/select_random_sentences.py
+++ util/select_random_sentences.py
@@ -3,7 +3,7 @@ import random
 text_file = open("resources/data.txt", "r")
 lines = text_file.readlines()
 
-random_list = random.sample(range(0, 10267), 100)
+random_list = random.sample(range(0, 999), 10)
 
 # store the following output in resources/data.txt
 for i in random_list:
