67b4e65faccf66ba05778dece51fb54acef9af73
Kunal Rout <2kunal6@gmail.com>
2024-03-10 12:37:05 +0530

    getting only doc

--- src/main.py
+++ src/main.py
@@ -12,5 +12,8 @@ for file in os.listdir('resources/semanticaly_similar_sentences'):
     lines = text_file.readlines()
     print(lines[0])
     for i in range(2,len(lines)):
-        print(faiss_search.search_similar_docs(lines[i], 1))
-        print(chroma_db.search_similar_docs(lines[i], 1))
+        similar_doc = faiss_search.search_similar_docs(lines[i], 1)
+        print(similar_doc[0].page_content)
+
+        similar_doc = chroma_db.search_similar_docs(lines[i], 1)
+        print(similar_doc['documents'][0][0])
